9556af5d67b712c2b2506a48d27e5d2009ea13e2
rubymaxioned <rubym@axioned.com>
2022-11-12 14:54:36 +0530

    pattern 4

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -23,3 +23,30 @@ for (var i = 0; i < n; i++) {
     }
     document.write("<br>");
 }
+
+
+//Pattern 4
+var n = 4;
+for (var i = 0; i <= n; i++) {
+    for (var j = 0; j <= (2 * n) + 1; j++) {
+        if ((i < j) && (j < ((n * 2) + 1) - i)) {
+            document.write("&nbsp&nbsp");
+        }
+        else {
+            document.write("*");
+        }
+    }
+    document.write("<br>");
+}
+
+for (var i = 0; i <= n; i++) {
+    for (var j = 0; j <= (2 * n) + 1; j++) {
+        if ((n - i < j) && (j < (n + 1) + i)) {
+            document.write("&nbsp&nbsp");
+        }
+        else {
+            document.write("*");
+        }
+    }
+    document.write("<br>");
+}
